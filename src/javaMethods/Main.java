a90e2087768420ca17fd156d458b001874d0e9de
unknown <earr0119@hotmail.com>
2022-09-20 20:57:59 -0400

    first commit in Methods class

---
+++ src/javaMethods/Main.java
@@ -0,0 +1,15 @@
+package javaMethods;
+
+public class Main {
+	
+public static void main(String[] args) {
+	
+	                //012345678901234567890123
+	String message = "we love Java programing";
+	
+	System.out.println(message.charAt(6));
+	System.out.println(message.length());
+	System.out.println(message.indexOf("J"));
+
+ }
+}
